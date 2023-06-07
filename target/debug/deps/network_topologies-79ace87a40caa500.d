17f31c99a8fc80ecaf10d0cae8527a45411077f0
BMcC59 <s2221528@ed.ac.uk>
2023-06-07 10:41:06 -0600

    Working on TransmissionLine Methods

--- target/debug/deps/network_topologies-79ace87a40caa500.d
+++ target/debug/deps/network_topologies-79ace87a40caa500.d
@@ -1,5 +1,5 @@
-/Users/bmcc/Documents/Programs/Rust/network_topologies/target/debug/deps/network_topologies-79ace87a40caa500: src/main.rs
+/Users/bmcc/Documents/Programs/Rust/Network_Topologies/target/debug/deps/network_topologies-79ace87a40caa500: src/main.rs
 
-/Users/bmcc/Documents/Programs/Rust/network_topologies/target/debug/deps/network_topologies-79ace87a40caa500.d: src/main.rs
+/Users/bmcc/Documents/Programs/Rust/Network_Topologies/target/debug/deps/network_topologies-79ace87a40caa500.d: src/main.rs
 
 src/main.rs:
