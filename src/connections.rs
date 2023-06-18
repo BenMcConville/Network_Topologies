c41dd71af72d6cc53a6a6c2c2884fa331882ce64
BenMcConville <benjamin.j.mcconville@gmail.com>
2023-06-18 18:36:29 -0500

    Analysis for network topologies.

--- src/connections.rs
+++ src/connections.rs
@@ -16,4 +16,21 @@ impl Connection {
     pub fn add_node(&mut self, node: Node)  {
         self.node_connections.push(node);
     }
+
+    pub fn cycle(&mut self)  {
+        for elem in self.node_connections   {
+            if elem.relay != 0  {
+                self.emit_update(elem.relay, elem.name_id);
+                elem.relay = 0;      
+            }
+        }
+    }
+    fn emit_update(&mut self, signal: u8, source_id: u8)    {
+        for elem in self.node_connections   {
+            if elem.name_id != source_id    {
+                elem.relay = signal;
+            }
+        }
+    }
 }
+
--- src/node.rs
+++ src/node.rs
@@ -1,9 +1,11 @@
 #[derive(Debug)]
 pub struct Node	{
 	pub name_id: u8,
+    pub relay: u8, //Replace with enum data later
 }
 pub fn new_node(id: u8) -> Node	{
 	Node {
 		name_id: id,
+        relay: 0,
 	}
 }
