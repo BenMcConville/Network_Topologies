a4845d5c7742d3eccff8001d352c36e09ce7539a
BenMcConville <benjamin.j.mcconville@gmail.com>
2023-06-24 11:15:54 -0500

    Signal propagation semiworking (starting to work on updates)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::io;
 ##   Network Topologies   ##
 ##########################*/
 
+/*---Updates
+- Add Hashmap for node/connection/network storage
+    - Connot access same node from different lines
+- Add single id parsing function (Network Module)
+- Add Network module
+*/
+
 
 /*	Structs
 ##########################*/
@@ -27,7 +34,7 @@ fn main()	{
 	let mut run = true;
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     while run   {
-        println!("Select an option: \n1- Add node to line\n2- Create Line\n3- View network");
+        println!("Select an option: \n1- Add node to line\n2- Create Line\n3- View network\n4- Send Signal");
         let mut userIn = String::new();
         io::stdin().read_line(&mut userIn).expect("NVI__");
         let userIn: u8 = match userIn.trim().parse() {
@@ -58,10 +65,45 @@ fn run_op(mat: &mut Vec<connections::Connection>, n: u8)    {
         1 => node_to_line(mat),
         2 => line_to_network(mat),
         3 => Display_AdjM(mat),
+        4 => Send_Signal(mat),
         _ => println!("Non Valid Response")
     }
     for line in mat {
         line.cycle();
+    }
+}
+
+fn Send_Signal(mat : &mut Vec<connections::Connection>) {
+    let mut line_id = String::new();
+    println!("Enter Signal Line Source");
+    io::stdin().read_line(&mut line_id).expect("NVI__");
+    let line_id: u8 = match line_id.trim().parse() {
+            Ok(num) => {
+                num
+            },
+            Err(_) => {
+               255 
+            },
+    };
+    let mut node_id = String::new();
+    println!("Enter Node Source");
+    io::stdin().read_line(&mut node_id).expect("NVI__");
+    let node_id: u8 = match node_id.trim().parse() {
+            Ok(num) => {
+                num
+            },
+            Err(_) => {
+               255 
+            },
+    };
+    for line in mat {
+        if line.name_id == line_id  {
+            for node in &mut line.node_connections   {
+                if node.name_id == node_id  {
+                    node.relay = 1;
+                }
+            }
+        }
     }
 }
 
